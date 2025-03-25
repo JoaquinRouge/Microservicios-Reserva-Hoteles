66478dfa2cc4db6c4f1d843321a75048ea73631d
JoaquinRouge <Joarouge@gmail.com>
2025-03-25 16:03:42 -0300

    Room and Reservation Microservices added. POM modifications in all services

---
+++ room-service/src/test/java/com/joaquinrouge/room_service/RoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaquinrouge.room_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
